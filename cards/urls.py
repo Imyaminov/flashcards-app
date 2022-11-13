1a3dba2944100dbe47b212f5facb045f521aa633
Imyaminov <250503iaa@gmail.com>
2022-11-13 11:09:11 +0300

    added view and template to add new card

--- cards/urls.py
+++ cards/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import CardListView
+from .views import CardListView, CardCreateView
 
 urlpatterns = [
     # path('', TemplateView.as_view(template_name='cards/home.html'), name='home')
-    path('', CardListView.as_view(), name='card-list')
+    path('', CardListView.as_view(), name='card-list'),
+    path('card/new', CardCreateView.as_view(), name='card-form'),
 ]
--- cards/views.py
+++ cards/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import (
     ListView,
+    CreateView,
 )
+from django.urls import reverse_lazy
 from .models import Card
 
 # Create your views here.
 
 class CardListView(ListView):
     model = Card
-    queryset = Card.objects.all().order_by('-box', 'created_at')
+    queryset = Card.objects.all().order_by('box', 'created_at')
+
+class CardCreateView(CreateView):
+    model = Card
+    fields = ('question', 'answer', 'box')
+    success_url = reverse_lazy('card-list')
